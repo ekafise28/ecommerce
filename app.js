3d2e121b3fe4579f76309a6b5438c0bf21ff187c
ekafise <ekafise.st@gmail.com>
2018-12-23 13:52:02 +0700

    tahap1

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var path = require('path'); //untuk path atau static page
+var mongoose = require('mongoose');
 
 //initial app
 var app = express();
 
+//koneksi database
+mongoose.connect('mongodb://localhost/ecommerce');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+    console.log('Sudah konek db');
+});
+
 //view engine
 app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set('view engine','ejs');   
 
 // setup public folder
 app.use(express.static(path.join(__dirname,'public')));
-//app.use(express.static(path.join(__dirname,'models')));
-//app.use(express.static(path.join(__dirname,'views')));
-//app.use(express.static(path.join(__dirname,'routes')));
 
 //index page
 app.get('/',function(req, res){
